61ea5851bc6733d7db791a29868f0aa77b59e315
Chris Whitney <info@e-commsolution.com>
2016-04-23 17:34:50 -0700

    start login

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -3,16 +3,11 @@ var React = require('react');
 var Actions = require('../actions');
 var Store = require('../stores');
 
-var Card = require('./card.jsx');
 var Signin = require('./signin.jsx');
-var MainContent = require('./main-content.jsx');
-var Navigation = require('./navigation.jsx');
 
 module.exports = React.createClass({
     getInitialState:function() {
-        return {
-            path: 'home'
-        };
+
     },
     componentDidMount: function() {
         this.unsubscribe = Store.listen(this.onChange);
@@ -21,14 +16,11 @@ module.exports = React.createClass({
         this.unsubscribe();
     },
     onChange: function() {
-        this.setState({
-            path: Store.getPath()
-        });
+        
     },
     render: function() {
         return (
             <div className="container">
-                <Navigation {...this.state}/>
                 <Signin />
             </div>
         );
