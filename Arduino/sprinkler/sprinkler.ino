542e90b7c6d93ff90c6c2bf6a62c9ecc1941f96c
Chris Whitney <info@e-commsolution.com>
2016-07-01 16:07:22 -0700

    found old code. score

--- Arduino/sprinkler/sprinkler.ino
+++ Arduino/sprinkler/sprinkler.ino
@@ -3,45 +3,43 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = ".......";
-const char* pass = ".......";
-const char* apiKey = ".......";
-String url = "http://chriswhitney.com/IoT/api/sprinkler.php?key=";
+const char* ssid = "Empire-2.4";
+const char* password = "5038038883";
 
 ESP8266WebServer server(80);
 
-const int led = 13;
+const int zoneOne = 0;
+const int zoneTwo = 2;
+
+const int zoneOneState = 0;
+const int zoneTwoState = 0;
+
+const long maxTime = 60000 * 30;
+
+const long zoneOneTimeout = 0;
+const long zoneTwoTimeout = 0;
+
+//void controllZoneOne(state) {
+//    zoneOneTimeout = millis();
+//}
+//
+//void controllZoneTwo(state) {
+//
+//     zoneTwoTimeout = millis();
+//}
 
 void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
-}
-
-void handleNotFound(){
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
+  String zone = server.arg("zone");
+  String state = server.arg("state");
+  String message = "hello from esp8266! Zone=" + zone + " state=" + state;
+  server.send(200, "text/plain", message);
 }
 
 void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("");
-
+  WiFi.softAP("esp8266-1", "nothingyoucanfindout", 1, 1);
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -58,12 +56,6 @@ void setup(void){
   }
 
   server.on("/", handleRoot);
-
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
 
   server.begin();
   Serial.println("HTTP server started");
